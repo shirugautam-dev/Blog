b4ccb16974cf70c78e4f913f0d42940d209fcd35
shirugautam-dev <shiru.gautam@gmail.com>
2026-04-09 16:35:17 +0530

    Add files via upload

--- js/books-data.js
+++ js/books-data.js
@@ -5,7 +5,7 @@ const books = [
     "slug": "book1",
     "excerpt": "This is my review on Britt_Mary_was_here",
     "rating": 1,
-    "cover": "../Images/Britt_Mary_was_here.jpg",
+    "cover": "Images/Britt_Mary_was_here.jpg",
     "date": "2026-04-09T00:00:00.000Z"
   },
   {
@@ -13,7 +13,7 @@ const books = [
     "slug": "book2",
     "excerpt": "This is my review on Book The Answer is No",
     "rating": 2,
-    "cover": "../Images/The_Answer_is_No.jpg",
+    "cover": "Images/The_Answer_is_No.jpg",
     "date": "2026-04-10T00:00:00.000Z"
   },
   {
@@ -21,7 +21,7 @@ const books = [
     "slug": "book3",
     "excerpt": "This is my review on Mother_Mary_Comes_to_Me",
     "rating": 3,
-    "cover": "../Images/Mother_Mary_Comes_to_Me.jpg",
+    "cover": "Images/Mother_Mary_Comes_to_Me.jpg",
     "date": "2026-04-11T00:00:00.000Z"
   },
   {
@@ -29,7 +29,7 @@ const books = [
     "slug": "book4",
     "excerpt": "This is my review on Friends",
     "rating": 4,
-    "cover": "../Images/Friends.jpg",
+    "cover": "Images/Friends.jpg",
     "date": "2026-09-12T00:00:00.000Z"
   }
 ];
