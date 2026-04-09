a4948c7b6fefbc188b043289973b1a3cdf0aef09
shirugautam-dev <shiru.gautam@gmail.com>
2026-04-09 16:08:23 +0530

    Add files via upload

---
+++ books-data.js
@@ -0,0 +1,39 @@
+
+const books = [
+  {
+    "title": "Britt_Mary_was_here",
+    "slug": "book1",
+    "excerpt": "This is my review on Britt_Mary_was_here",
+    "rating": 1,
+    "cover": "Images/Britt_Mary_was_here.jpg",
+    "date": "2026-04-09T00:00:00.000Z"
+  },
+  {
+    "title": "The_Answer_is_No",
+    "slug": "book2",
+    "excerpt": "This is my review on Book The Answer is No",
+    "rating": 2,
+    "cover": "Images/The_Answer_is_No.jpg",
+    "date": "2026-04-10T00:00:00.000Z"
+  },
+  {
+    "title": "Mother Mary Comes to Me",
+    "slug": "book3",
+    "excerpt": "This is my review on Mother_Mary_Comes_to_Me",
+    "rating": 3,
+    "cover": "Images/Mother_Mary_Comes_to_Me.jpg",
+    "date": "2026-04-11T00:00:00.000Z"
+  },
+  {
+    "title": "Friends",
+    "slug": "book4",
+    "excerpt": "This is my review on Friends",
+    "rating": 4,
+    "cover": "Images/Friends.jpg",
+    "date": "2026-09-12T00:00:00.000Z"
+  }
+];
+
+if (typeof module !== "undefined") {
+  module.exports = books;
+}
