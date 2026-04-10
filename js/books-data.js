cbd096908495e7219f0856d83d4eb7b638d612a6
shirugautam-dev <shiru.gautam@gmail.com>
2026-04-10 14:57:01 +0530

    Add files via upload

---
+++ js/books-data.js
@@ -0,0 +1,39 @@
+
+const books = [
+  {
+    "title": "BRITT-MARIE WAS HERE",
+    "slug": "book1",
+    "excerpt": "This is my review on Britt_Mary_was_here",
+    "rating": 1,
+    "cover": "Images/Britt_Mary_was_here.jpg",
+    "date": "2026-04-09T00:00:00.000Z"
+  },
+  {
+    "title": "THE ANSWER IS NO",
+    "slug": "book2",
+    "excerpt": "This is my review on Book The Answer is No",
+    "rating": 2,
+    "cover": "Images/The_Answer_is_No.jpg",
+    "date": "2026-04-10T00:00:00.000Z"
+  },
+  {
+    "title": "Mother Mary Comes To Me",
+    "slug": "book3",
+    "excerpt": "This is my review on Mother_Mary_Comes_to_Me",
+    "rating": 3,
+    "cover": "Images/Mother_Mary_Comes_to_Me.jpg",
+    "date": "2026-04-11T00:00:00.000Z"
+  },
+  {
+    "title": "MY FRIENDS",
+    "slug": "book4",
+    "excerpt": "This is my review on Friends",
+    "rating": 4,
+    "cover": "Images/Friends.jpg",
+    "date": "2026-09-12T00:00:00.000Z"
+  }
+];
+
+if (typeof module !== "undefined") {
+  module.exports = books;
+}
