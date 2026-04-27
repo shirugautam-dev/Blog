022eed1a4e37e97e37537e0be978ab09199ddf2a
shirugautam-dev <shiru.gautam@gmail.com>
2026-04-27 16:00:30 +0530

    Add files via upload

--- js/articles.js
+++ js/articles.js
@@ -125,6 +125,25 @@ const container = document.getElementById("articles");
         <p class="post-meta">
           Published in Medium on ${formattedDate}
         </p>
+          <div class="share-container">
+  <span class="share-label">Share:</span>
+
+  <a href="https://twitter.com/intent/tweet?url=${url}&text=${article.title}" target="_blank" class="share-btn">
+    <i class="fa-brands fa-x-twitter"></i>
+  </a>
+
+  <a href="https://www.linkedin.com/sharing/share-offsite/?url=${url}" target="_blank" class="share-btn">
+    <i class="fa-brands fa-linkedin"></i>
+  </a>
+
+  <a href="https://www.facebook.com/sharer/sharer.php?u=${url}" target="_blank" class="share-btn">
+    <i class="fa-brands fa-facebook"></i>
+  </a>
+
+  <a href="https://wa.me/?text=${url}" target="_blank" class="share-btn">
+    <i class="fa-brands fa-whatsapp"></i>
+  </a>
+</div>
       `;
 
       container.appendChild(post);
